7c56d074a0c115324b8844ca6a669899573d2aeb
rizwana25 <fathimathrizwana761@gmail.com>
2026-06-15 22:43:35 +0530

    Added projects section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -85,7 +85,7 @@ function Skills() {
         </h2>
 
         {/* Skills Grid */}
-        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
 
           {mainSkills.map((skill, index) => (
             <div
