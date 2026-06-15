f8f3d87558d29ff02789091f7c7b03c68843a946
rizwana25 <fathimathrizwana761@gmail.com>
2026-06-15 19:16:45 +0530

    Added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Certificates from "./components/Certificates";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Projects from "./components/project";
+import { FaWhatsapp } from "react-icons/fa";
 
 
 function App() {
@@ -28,6 +29,30 @@ function App() {
 
     </>
   );
+  <a
+  href="https://wa.me/918139055063"
+  target="_blank"
+  rel="noreferrer"
+  className="
+    fixed
+    bottom-5
+    right-5
+    w-14
+    h-14
+    bg-[#8FA684]
+    text-white
+    rounded-full
+    shadow-lg
+    flex
+    items-center
+    justify-center
+    hover:scale-110
+    transition
+    z-50
+  "
+>
+  <FaWhatsapp size={28} />
+</a>
 }
 
 export default App;
